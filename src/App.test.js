90b75409ac447216cf741b9e16ec5ee09fe354d8
ninasjoberg <nina.sjoberg@gmail.com>
2018-01-21 23:04:41 +0100

    test setup

--- src/App.test.js
+++ src/App.test.js
@@ -7,3 +7,20 @@ it('renders without crashing', () => {
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
+
+
+
+/* import { shallow } from 'enzyme';
+
+it('renders without crashing', () => {
+   shallow(<App />);
+ });
+
+ Unlike the previous smoke test using ReactDOM.render(),
+ this test only renders <App> and doesn’t go deeper.
+ even if <App> itself renders a <Button> that throws, this test will pass.
+
+ Shallow rendering is great for isolated unit tests,
+ but you may still want to create some full rendering tests to ensure the
+ components integrate correctly. Enzyme supports full rendering with mount(),
+ and you can also use it for testing state changes and component lifecycle.*/
